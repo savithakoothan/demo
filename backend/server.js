e671c9678304d1034656c014933a6a3febf3a851
savitha <savitha@designqubits.com>
2026-04-10 11:41:16 +0530

    Fixing bug

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.js";
-import path from path;
+import path from "path";
 import { fileURLToPath } from "url";
 
 // Required in ES6 modules (__dirname is not available by default)
